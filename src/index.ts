6a3307a0e2d188d3400b491f8c71d107ec4666fa
Sema García <jose-manuel.garcia@gft.com>
2017-09-13 20:54:23 +0200

    Add minimal structure

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// import 'core-js'; (npm i -D core-js)
+
+export * from './class-decorators';
+export * from './method-decorators';
+export * from './parameter-decorators';
+export * from './property-decorators';
