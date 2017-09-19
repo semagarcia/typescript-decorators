a16e761a644071a05f98b87c992be49856894fe4
Sema García <jose-manuel.garcia@gft.com>
2017-09-19 23:19:25 +0200

    Comment empty decorator folders (parameter & property)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+// Polyfills
 // import 'core-js'; (npm i -D core-js)
 
 export * from './class-decorators';
 export * from './method-decorators';
-export * from './parameter-decorators';
-export * from './property-decorators';
+//export * from './parameter-decorators';
+//export * from './property-decorators';
