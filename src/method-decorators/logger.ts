b8a259ad2b1fb07d047335f5b544cda889a517f8
Sema García <jose-manuel.garcia@gft.com>
2017-09-21 01:08:45 +0200

    Add logger for methods

--- src/method-decorators/logger.ts
+++ src/method-decorators/logger.ts
@@ -1,3 +1,5 @@
+import { printTimeStamp } from './../utils';
+
 export interface LoggerMethodAnnotation {
     entryTrace?: boolean;
     endTrace?: boolean;
@@ -19,7 +21,8 @@ export function LoggerMethod(loggerOptions?: LoggerMethodAnnotation): MethodDeco
     const prefix = (loggerOptions && loggerOptions.prefix) ? (loggerOptions.prefix + ' ') : '';
     const timestamp = loggerOptions && !!loggerOptions.timestamp;
 
-    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    // descriptor: TypedPropertyDescriptor<Function>
+    return function(target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
         // save a reference to the original method this way we keep the values currently in the
         // descriptor and don't overwrite what another decorator might have done to the descriptor.
         if(descriptor === undefined) {
@@ -59,21 +62,3 @@ export function LoggerMethod(loggerOptions?: LoggerMethodAnnotation): MethodDeco
     }
 }
 
-/**
- * 
- * Print a timestamp date if the following format: dd/MM/YYYY HH:mm:ss
- * 
- * @returns 
- */
-function printTimeStamp() {
-    const now = new Date();
-    return ''.concat('[')
-            .concat(`${now.getDate()}`)
-            .concat(`/${(now.getMonth()+1 < 10) ? ("0" + (now.getMonth()+1)) : now.getMonth()+1}`)
-            .concat(`/${now.getFullYear()}`)
-            .concat(' ')
-            .concat(`${(now.getHours() < 10) ? "0" + now.getHours() : now.getHours()}`)  // Hours
-            .concat(`:${(now.getMinutes() < 10) ? "0" + now.getMinutes() : now.getMinutes()}`)  // Mins
-            .concat(`:${(now.getSeconds() < 10) ? "0" + now.getSeconds() : now.getSeconds()}`)  // Secs
-            .concat('] ');
-}
