f644b7fb2e84f73525421cd1b9eec328222e218c
Sema García <jose-manuel.garcia@gft.com>
2017-09-24 23:11:27 +0200

    Add unit cases

--- test/class-decorators/logger.spec.ts
+++ test/class-decorators/logger.spec.ts
@@ -1,13 +1,34 @@
 import { LoggerClass } from './../../src/';
 
-describe("HelloComponent", () => {
+describe('LoggerClass decorator', () => {
 
     @LoggerClass() class X {}
 
-    it("should say 'Hello world!'", () => {
+    it('should log class with no annotation', () => {
 
         let x = new X();
 
         expect(true).toBe(true);
     });
+
+    it('should log class with null annotation', () => {
+
+    });
+
+    it('should log class with prefix', () => {
+
+    });
+
+    it('should log class with timestamp', () => {
+
+    });
+
+    it('should log class with message', () => {
+
+    });
+
+    it('should log class with all annotation values', () => {
+
+    });
+
 });
