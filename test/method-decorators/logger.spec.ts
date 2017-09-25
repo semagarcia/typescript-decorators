db10edc0476366ae305993644e4a1f7ee0f0462c
Sema García <jose-manuel.garcia@gft.com>
2017-09-25 02:58:09 +0200

    Update test cases for method logger decorator

--- test/method-decorators/logger.spec.ts
+++ test/method-decorators/logger.spec.ts
@@ -1,20 +1,89 @@
 import { LoggerMethod } from './../../src/';
 
-describe("HelloComponent", () => {
+describe('LoggerMethod decorator', () => {
 
-    class X {
-        @LoggerMethod()
-        add(a, b) {
-            return a + b; 
-        }
-    }
-
-    it("should log method calls", () => {
-
-        let x = new X();
-        x.add(2, 3);
-        x.add(1, 5);
-
-        expect(true).toBe(true);
+    beforeEach(() => {
+        spyOn(console, 'log');
     });
+
+    it('should not output log trace (no annotation, empty, default behaviour)', () => {
+        class TestClassMethod {
+        @LoggerMethod()
+            add(a, b) {
+                return a + b; 
+            }
+        }
+
+        let testClass = new TestClassMethod();
+        testClass.add(2, 3);
+        expect(console.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not output log trace (null annotation, default behaviour)', () => {
+        class TestClassMethod {
+        @LoggerMethod(null)
+            add(a, b) {
+                return a + b; 
+            }
+        }
+
+        let testClass = new TestClassMethod();
+        testClass.add(2, 3);
+        expect(console.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should output default log trace for method start', () => {
+        class TestClassMethod {
+        @LoggerMethod({ entryTrace: true })
+            add(a, b) {
+                return a + b; 
+            }
+        }
+
+        let testClass = new TestClassMethod();
+        testClass.add(2, 3);
+        expect(console.log).toHaveBeenCalledTimes(2);
+    });
+
+    it('should output default log trace in the method end', () => {
+        class TestClassMethod {
+        @LoggerMethod({ endTrace: true })
+            add(a, b) {
+                return a + b; 
+            }
+        }
+
+        let testClass = new TestClassMethod();
+        testClass.add(2, 3);
+        expect(console.log).toHaveBeenCalledTimes(3);
+    });
+
+    it('should output default log trace in the beginning and end method invocation', () => {
+        class TestClassMethod {
+        @LoggerMethod({ entryTrace: true, endTrace: true })
+            add(a, b) {
+                return a + b; 
+            }
+        }
+
+        let testClass = new TestClassMethod();
+        testClass.add(2, 3);
+        expect(console.log).toHaveBeenCalledTimes(4);
+    });
+
+    it('should output log trace with custom prefix', () => {
+        let customPrefix = '[TEST-PREFIX]';
+        class TestClassMethod {
+        @LoggerMethod({ prefix: customPrefix })
+            add(a, b) {
+                return a + b; 
+            }
+        }
+
+        let testClass = new TestClassMethod();
+        testClass.add(2, 3);
+        expect(console.log).toHaveBeenCalledTimes(1);
+        expect(console.log).toHaveBeenCalledWith(jasmine.any(String));
+    });
+
 });
