058b1022a6f0bf6f5cad4202dc42a596f2c9bf0c
Sema García <jose-manuel.garcia@gft.com>
2017-10-09 22:25:39 +0200

    Update email implementation

--- src/property-decorators/email.ts
+++ src/property-decorators/email.ts
@@ -1,9 +1,13 @@
 export interface EmailOptions {
+    protect?: boolean;
     throwError?: boolean;
 }
 
 export function Email(emailOptions?: EmailOptions) {
+    const protect = (emailOptions && emailOptions.protect) ? emailOptions.protect : true;
+    const throwError = (emailOptions && emailOptions.throwError) ? emailOptions.throwError : false;
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     return function(target: Object, propertyKey: string | symbol) {
         let value = target[propertyKey];
         Reflect.deleteProperty[propertyKey];
@@ -13,14 +17,12 @@ export function Email(emailOptions?: EmailOptions) {
 
             // Setter
             set: (val) => {
-                console.log('Valid? ', regex.test(val));
-                /*if(val <= maxValue) {
-                    // The value is less or equals
-                    value = val;
+                if(regex.test(val)) {
+                    val = value;
                 } else {
                     if(!protect) value = null;
-                    if(throwError) throw new Error(throwError);
-                }*/
+                    if(throwError) throw new Error('Invalid email address format');
+                }
             }
         });
     }
