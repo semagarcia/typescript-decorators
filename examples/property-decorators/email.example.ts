f042f38d862f35141325559ed0e4726fa86b3ab8
Sema García <jose-manuel.garcia@gft.com>
2017-10-10 16:56:25 +0200

    Update example with more use cases

--- examples/property-decorators/email.example.ts
+++ examples/property-decorators/email.example.ts
@@ -5,8 +5,30 @@ export class EmailExampleClass {
     @Email()
     simpleEmail: string;
 
+    @Email({ protect: false })
+    nonProtectedEmail: string;
+
+    @Email({ throwError: true })
+    throwableEmail: string;
+
 }
 
 let exampleClass = new EmailExampleClass();
 exampleClass.simpleEmail = 'a@a.com';
+console.log('\nAssiging a valid address: ', exampleClass.simpleEmail);
 exampleClass.simpleEmail = 'a.@a.com';
+console.log('Assiging an invalid address (default annotations): ', exampleClass.simpleEmail);
+
+exampleClass.nonProtectedEmail = 'a@a.com';
+console.log('\nAssiging a valid address: ', exampleClass.nonProtectedEmail);
+exampleClass.nonProtectedEmail = 'a.@a.com';
+console.log('Assiging an invalid address (with protect = false): ', exampleClass.nonProtectedEmail);
+
+exampleClass.throwableEmail = 'a@a.com';
+console.log('\nAssiging a valid address: ', exampleClass.throwableEmail);
+try {
+    exampleClass.throwableEmail = 'a.@a.com';
+    console.log('Assiging an invalid address (with throwError = true): ', exampleClass.throwableEmail);
+} catch(error) {
+    console.log('Exception catched!!');
+}
